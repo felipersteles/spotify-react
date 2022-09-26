0cb755752d4f71d82e2feda6def8ec5966f30ce1
Felipe Teles <felipersteles@gmail.com>
2022-09-25 23:38:47 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import {Login, Spotify} from './components';
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
+
+function App() {
+  const [{ token }, dispatch] = useStateProvider();
+  let acesso = false;
+
+  useEffect(() => {
+    const hash = window.location.hash;
+
+    console.log('hash: ' + hash)
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      //console.log(token);
+
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div>
+      {
+        //se a token for vazia vai para o login
+        acesso ? <Spotify /> : <Login />
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/spotify-item/index.js
@@ -0,0 +1,5 @@
+
+export { default as Body } from './Body';
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as Footer } from './Footer';
